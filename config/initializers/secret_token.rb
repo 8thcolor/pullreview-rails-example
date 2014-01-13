0aaae387428b506f09f0b5a1058dcc8d5c878ec7
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2014-01-13 23:30:44 +0100

    Static pages: Document secure token method

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -12,6 +12,10 @@
 
 require 'securerandom'
 
+
+# Public: Generate a secure token randomly.
+#
+# Returns the generated token.
 def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
